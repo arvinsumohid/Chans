62b8f7f5be41dde25f1f8635faf2a44b88af60cb
arvinsumohid <arvinsumohid99@gmail.com>
2026-01-29 01:32:08 +0800

    update

--- src/pages/doctor/index.jsx
+++ src/pages/doctor/index.jsx
@@ -1,22 +1,26 @@
 import { useState } from 'react'
-import { Box, Button } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 import ProtectedRoute from '../../routes/ProtectedRoute'
 import DoctorsList from '../../components/tables/DoctorsList'
 import DoctorForm from '../../components/forms/DoctorForm'
 import { PrimaryThemeColor } from '../../utils/constant'
 import { LoadListProvider } from '../../contexts/LoadListContext';
+import MedicalPersonPopup from '../../components/popup/MedicalPersonPopup'
 
 const DoctorPage = () => {
-  const [isAddDoctor, setIsAddDoctor] = useState(false);
   const [loadList, setLoadList] = useState(false);
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
   return (
     <LoadListProvider>
       <Box className="flex flex-col gap-4">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+              <Typography variant="h6">Medical Personnel</Typography>
+              <Button variant="contained" sx={{ textTransform: 'none', ...PrimaryThemeColor }} onClick={handleOpen}>Add medical personnel</Button>
+          </Box>
         <DoctorsList loadList={loadList} setLoadList={setLoadList} />
-        <Box sx={{ mt: 2 }}>
-          <Button variant="contained" sx={{ textTransform: 'none', ...PrimaryThemeColor }} onClick={() => setIsAddDoctor(true)}>Add medical personnel</Button>
-        </Box>
-        {isAddDoctor && <DoctorForm setLoadList={setLoadList} onClose={() => setIsAddDoctor(false)} />}
+        <MedicalPersonPopup open={open} handleClose={handleClose} />
       </Box>
     </LoadListProvider>
   )
